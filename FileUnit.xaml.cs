e505c048a414a343b23e694558ea0445875ae77c
p.gorovenko <watermelonrytp@gmail.com>
2022-02-22 18:40:54 +0300

    file user controller

--- FileUnit.xaml.cs
+++ FileUnit.xaml.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace TheoryOfInformation.lab1
 {
@@ -23,6 +17,23 @@ namespace TheoryOfInformation.lab1
         public FileUnit()
         {
             InitializeComponent();
+        }
+
+        private void InputFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Multiselect = false,
+                Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*",
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
+            };
+            if (openFileDialog.ShowDialog() == true)
+            {
+                foreach (string filename in openFileDialog.FileNames)
+                {
+                    ((Button)sender).Tag = filename;
+                }
+            }
         }
     }
 }
