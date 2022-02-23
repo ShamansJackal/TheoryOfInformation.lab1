3fe8baedc815cd9b065f3e55113b1237cf392134
p.gorovenko <watermelonrytp@gmail.com>
2022-02-23 16:25:36 +0300

    Proto layers

---
+++ Encryptions/TextCleaner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using TheoryOfInformation.lab1.Structs;
+
+namespace TheoryOfInformation.lab1.Encryptions
+{
+    public delegate string Operation(string key, string text);
+
+    public static class TextCleaner
+    {
+        public static string WorkWithText(string key, string text, Operation operation)
+        {
+            List<RemovedSymbl> removedSymbls = CleanText(ref text);
+
+            string encodedTxt = operation(key, text);
+
+            return ReturnText(encodedTxt, removedSymbls);
+        }
+
+        private static List<RemovedSymbl> CleanText(ref string text)
+        {
+            return default;
+        }
+
+        private static string ReturnText(string text, List<RemovedSymbl> removedSymbls)
+        {
+            return text;
+        }
+    }
+}
---
+++ Structs/RemovedSymbl.cs
@@ -0,0 +1,14 @@
+﻿namespace TheoryOfInformation.lab1.Structs
+{
+    public struct RemovedSymbl
+    {
+        public int index;
+        public char symble;
+
+        public RemovedSymbl(int ind, string str)
+        {
+            index = ind;
+            symble = str[ind];
+        }
+    }
+}
