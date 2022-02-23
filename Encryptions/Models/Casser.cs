47b96adb44432acaa2be4ab9a8675e4123430a7a
p.gorovenko <watermelonrytp@gmail.com>
2022-02-23 15:53:00 +0300

    Encriptions interfaces

---
+++ Encryptions/Models/Casser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheoryOfInformation.lab1.Encryptions.Models
+{
+    internal class Casser : IEncryption
+    {
+        public bool HasKey => false;
+
+        public string Decrypte(string text, string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Encrypte(string text, string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return "Цезарь";
+        }
+    }
+}
---
+++ Encryptions/Models/IEncryption.cs
@@ -0,0 +1,10 @@
+﻿namespace TheoryOfInformation.lab1.Encryptions.Models
+{
+    internal interface IEncryption
+    {
+        string Encrypte(string text, string key);
+        string Decrypte(string text, string key);
+
+        bool HasKey { get; }
+    }
+}
