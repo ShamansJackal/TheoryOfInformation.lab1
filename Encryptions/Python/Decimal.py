592b4f3ff7a968bc4137d736a89606abc7686a00
p.gorovenko <watermelonrytp@gmail.com>
2022-02-24 16:51:56 +0300

    tmp

--- Encryptions/Python/Decimal.py
+++ Encryptions/Python/Decimal.py
@@ -1,1 +1,15 @@
-﻿
+﻿from Alpha import Langs
+
+def Encrypte(text, key, lang):
+    if not IsPrimary(int(key), Langs[lang].count):
+        return ""
+    return Langs[lang][2]
+
+
+def IsPrimary(a, b):
+    while a and b:
+        if a > b:
+            a = a % b
+        else:
+            b = b % a
+    return a+b==1
