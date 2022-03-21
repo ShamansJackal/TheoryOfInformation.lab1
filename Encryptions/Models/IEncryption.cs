341485369a9a6ac5946f1f5a12393686b3f57525
p.horovenko <p.horovenko@LINKER-W3>
2022-03-21 15:11:12 +0300

    Fix encriptyions

--- Encryptions/Models/IEncryption.cs
+++ Encryptions/Models/IEncryption.cs
@@ -7,5 +7,6 @@ namespace TheoryOfInformation.lab1.Encryptions.Models
         string Encrypte(string text, string key);
         string Decrypte(string text, string key);
         LangIds Lang { get; }
+        LangIds KeyLang { get; }
     }
 }
